17f1e39593842f2f3bf91f50f81ce2e463900484
fedor-jedi <fedor.contact@yandex.ru>
2021-11-02 13:09:31 +0300

    Исправлен импорт

--- HomeWork_2/WorkChat/src/main/java/com/theost/workchat/ui/fragments/DialogFragment.kt
+++ HomeWork_2/WorkChat/src/main/java/com/theost/workchat/ui/fragments/DialogFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.material.snackbar.Snackbar
